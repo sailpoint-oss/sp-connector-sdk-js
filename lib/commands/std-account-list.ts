42738a2f7f40b9dbe9f6f7151fe2f1b910b351e8
fangming-ning-sp <42616890+fangming-ning-sp@users.noreply.github.com>
2023-01-30 18:18:26 -0600

    Add state into input

--- lib/commands/std-account-list.ts
+++ lib/commands/std-account-list.ts
@@ -3,10 +3,22 @@
 import { Attributes, ObjectOutput } from './command'
 
 /**
+ * Input object of `std:account:list` command
+ */
+export type StdAccountListInput = {
+	stateful?: boolean
+	state?: StdAccountListState
+}
+
+/**
  * Output object of `std:account:list` command
  */
 export type StdAccountListOutput = ObjectOutput & {
 	disabled?: boolean
 	locked?: boolean
 	attributes: Attributes
+}
+
+export type StdAccountListState = {
+	[key: string]: any
 }
--- lib/handler.ts
+++ lib/handler.ts
@@ -24,6 +24,7 @@ import {
 	StdTestConnectionOutput,
 	StdChangePasswordInput,
 	StdChangePasswordOutput,
+	StdAccountListInput,
 } from './commands'
 import { Response } from './response'
 
@@ -67,8 +68,8 @@ export type StdAccountEnableHandler = (
 ) => Promise<void>
 export type StdAccountListHandler = (
 	context: Context,
-	input: undefined,
-	res: Response<StdAccountListOutput>
+	input: StdAccountListInput,
+	res: Response<StdAccountListOutput | StdAccountEnableOutput>
 ) => Promise<void>
 export type StdAccountReadHandler = (
 	context: Context,
