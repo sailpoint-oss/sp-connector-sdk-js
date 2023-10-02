c72f0627bd904ffb48516879ced409b1bd9f3fc1
Bob Potter <bob.potter@sailpoint.com>
2023-10-02 17:03:08 -0500

    PLTCONN-3953: fix connector spec read

--- lib/connector-spec.ts
+++ lib/connector-spec.ts
@@ -4,13 +4,22 @@ import { Response } from './response'
 import { Context } from './connector-handler'
 
 import { promisify } from 'util'
-import { readFile } from 'fs'
+import { readFile, existsSync } from 'fs'
+import path from 'path'
 
 const specFilePath = "connector-spec.json"
 
 export const StdSpecReadDefaultHandler = async (_context: Context, _input: StdSpecReadInput, res: Response<StdSpecReadOutput>) => {
-	const data = await promisify(readFile)(specFilePath)
-	res.send({
-		specification: JSON.parse(data.toString())
-	})
+	// Read from CWD if exists, otherwise check if it is in the same directory as this file
+	if (existsSync(specFilePath)) {
+		const data = await promisify(readFile)(specFilePath)
+		res.send({
+			specification: JSON.parse(data.toString())
+		})
+	} else {
+		const data = await promisify(readFile)(path.join(__dirname, specFilePath))
+		res.send({
+			specification: JSON.parse(data.toString())
+		})
+	}
 }
