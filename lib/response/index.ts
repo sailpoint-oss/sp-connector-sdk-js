e4e375518fc9be8f21e1d86629568d44c25c9ec1
fangming-ning-sp <fangming.ning@sailpoint.com>
2023-12-27 08:59:22 -0600

    PLTCONN-4268: Patch config

--- lib/response/index.ts
+++ lib/response/index.ts
@@ -9,6 +9,7 @@ export interface Response<T> {
 	send(output: T): void
 	saveState(state: any): void
 	keepAlive(): void
+	patchConfig(patches: Patch[]): void
 }
 
 /**
@@ -47,6 +48,10 @@ export class ResponseStream<T> implements Response<T> {
 	keepAlive(): void {
 		this._writable.write(new RawResponse({}, ResponseType.KeepAlive))
 	}
+
+	patchConfig(patches: Patch[]): void {
+		this._writable.write(new RawResponse(patches, ResponseType.Config))
+	}
 }
 
 /**
@@ -55,7 +60,8 @@ export class ResponseStream<T> implements Response<T> {
 export enum ResponseType {
 	Output = 'output',
 	State = 'state',
-	KeepAlive = 'keepAlive'
+	KeepAlive = 'keepAlive',
+	Config = 'config'
 }
 
 /**
@@ -70,3 +76,17 @@ export class RawResponse {
 		this.type = type
 	}
 }
+
+
+export type Patch = { op: string, path: string, value: any }
+
+// export class Patch {
+// 	op: string
+// 	path: string
+// 	value: any
+
+//     constructor(data: any, type = ResponseType.Config) {
+//        this.data = data
+// 		this.type = type
+// 	}
+// }
