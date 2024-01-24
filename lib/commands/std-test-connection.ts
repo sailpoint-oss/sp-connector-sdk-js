6860ab6e4eebda3d3b84317ec6420ed6b96f4889
fangming-ning-sp <fangming.ning@sailpoint.com>
2024-01-24 16:26:34 -0600

    PLTCONN-4499: Account schema for test connection

--- lib/commands/std-test-connection.ts
+++ lib/commands/std-test-connection.ts
@@ -1,5 +1,14 @@
 /* Copyright (c) 2021. SailPoint Technologies, Inc. All rights reserved. */
 
+import { AccountSchema } from './command'
+
+/**
+ * Input object of `std:test-connection` command
+ */
+export type StdTestConnectionInput = {
+	account_schema?: AccountSchema
+}
+
 /**
  * Output object of `std:test-connection` command
  */
