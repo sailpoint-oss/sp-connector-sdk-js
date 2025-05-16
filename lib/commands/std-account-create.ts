2738f434df9b428330644e0fbaf7895dfaddfb13
gaurav-wadekar-sp <gaurav.wadekar@sailpoint.com>
2025-05-16 14:36:13 +0530

    adding type of map

--- lib/commands/std-account-create.ts
+++ lib/commands/std-account-create.ts
@@ -8,7 +8,7 @@ import { AccountSchema, Attributes, ObjectOutput, Result } from './command'
 export type CreateAttributeWithMetadata = {
 	attribute: string // Attribute from account schema
 	value: any // Undefined for "Remove" op
-	metadata: Map<string, any>
+	metadata: Map<string, object>
 }
 
 /**
--- lib/commands/std-account-update.ts
+++ lib/commands/std-account-update.ts
@@ -15,7 +15,7 @@ export type AttributeChange = {
 	op: AttributeChangeOp
 	attribute: string // Attribute from account schema
 	value: any // Undefined for "Remove" op
-	metadata: Map<string, any>
+	metadata: Map<string, object>
 }
 
 /**
