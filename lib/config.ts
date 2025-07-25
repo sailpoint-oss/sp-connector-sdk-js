4a9da940cb6f523f625095bea1e72a8d10385327
Francisco Zamudio <francisco.zamudio@sailpoint.com>
2025-07-24 20:38:04 -0600

    PLTCONN-7831: store config in module variable

--- lib/config.ts
+++ lib/config.ts
@@ -6,6 +6,7 @@ interface configState {
 }
 
 const _configState = new AsyncLocalStorage<configState>();
+let _config: any = undefined;
 
 /**
  * Reads in connector config
@@ -16,10 +17,18 @@ export const readConfig = async (): Promise<any> => {
 		return store.cfg;
 	}
 
+	console.log("fco: calling custom readConfig");
+	if (_config != undefined) {
+		return _config;
+	}
+
 	const config = process.env['CONNECTOR_CONFIG']
 	if (!config) {
 		throw new Error(`unexpected runtime error: missing connector config`)
 	}
+
+	_config = config;
+	process.env['CONNECTOR_CONFIG'] = undefined;
 
 	try {
 		return JSON.parse(Buffer.from(config, 'base64').toString())
