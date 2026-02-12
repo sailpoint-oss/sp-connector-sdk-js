7b8578bafb7600dd4a95f3c13edfdb07d5606a05
Bob Potter <bob.potter@sailpoint.com>
2026-02-12 11:14:12 -0600

    sup

--- lib/logger/logger.ts
+++ lib/logger/logger.ts
@@ -24,7 +24,6 @@ export const logger = pino({
 		messageKey: 'message',
     level: logLevel(),
     base:undefined,
-		transport: pino.transport({target: 'pino/file'}),
 		formatters: {
 			level: (label: string) => {
 				return { level: label.toUpperCase() }
@@ -47,5 +46,5 @@ export const logger = pino({
     mixinMergeStrategy(mergeObject:any, mixinObject:any) {
         return {...mergeObject, ...mixinObject}
     }
-});
+}, pino.transport({target: 'pino/file'}));
 //}, pino.destination({sync: process.env["PINO_DEST_SYNC"] === 'true'}))
